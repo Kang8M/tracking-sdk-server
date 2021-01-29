e1896c58537baaeb4b4e49f48c3c79fb1e79443f
hung.luu <hung.luu@ntq-solution.com.vn>
2021-01-29 16:07:59 +0700

    update post ep

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res) {
   res.send('Hello world!');
 });
 
+app.post('/alm', function(req, res) {
+  let params = req.params;
+  res.send(params);
+});
+
 app.listen(port, function() {
   console.log(`Example app listening at http://localhost:${port}`);
 });
