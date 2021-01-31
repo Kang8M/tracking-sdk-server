98665120d96a5bbd99093da2a826814c534aa6fb
Kang8M <git@github.com>
2021-01-31 23:23:57 +0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors({ origin: '*' }));
+app.use(cors({ origin: '*' }));
 
 app.use(bodyParser.urlencoded({
   extended: true
